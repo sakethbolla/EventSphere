4d0683f46d6fd45f0ef1aeb79595b1a798c84762
baelul <hailebaelul@gmail.com>
2025-12-09 14:06:55 -0500

    code quality fixes

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfill for TextEncoder/TextDecoder which are needed by react-router v7
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
